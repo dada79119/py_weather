badf5864f7d564224c5820a7953ddfa97fe08ca1
dada79119 <dada79119@gmail.com>
2017-07-14 10:12:58 +0800

    weather.py
    Demo is handle weather data from 中央氣象局API

---
+++ weather.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+def weather(cityname):
+    cityname=cityname.replace('台','臺')    
+    key="{{Your Api Key}}"
+    dataid="{{data id}}"
+    api="http://opendata.cwb.gov.tw/opendataapi?dataid="+dataid+"&authorizationkey="+key
+    print(api)
+    print('Start parsing weather ...')
+    rs = requests.session()
+    res = rs.get(api, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    dic={}
+    i=0
+    for city in soup.select('locationname'):
+        dic[city.string]=i
+        i+=15
+
+    idx=dic[cityname]
+    content =""
+    wstatus = soup.select('parametername')[idx]
+    low_temp = soup.select('parametername')[idx+6]
+    high_temp = soup.select('parametername')[idx+3]
+    comfor = soup.select('parametername')[idx+9]
+    rate = soup.select('parametername')[idx+12]
+    
+    content += cityname + '\n' + '天氣概況:' + wstatus.string + '\n'
+    content += '氣溫:' + low_temp.string + '°C~' + high_temp.string + '°C \n'
+    content += '舒適度:' + comfor.string + '\n'
+    content += '降雨機率:' + rate.string + '%'    
+    return content
+
+weather('台北市')
